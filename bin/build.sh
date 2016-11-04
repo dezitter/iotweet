469759c0e04c5a8c82747fee4f490a5d4ea93ffd
Laurent Dezitter <laurent.dezitter@gmail.com>
2016-11-04 21:19:56 +0100

    minify bundle when in production environment

--- bin/build.sh
+++ bin/build.sh
@@ -3,8 +3,13 @@
 CLIENT_JS_DIR=client/js
 DIST_JS_DIR=dist/js
 
+NODE_MODULES_DIR=node_modules/
+BROWSERIFY_BIN="$NODE_MODULES_DIR/browserify/bin/cmd.js"
+UGLIFY_BIN="$NODE_MODULES_DIR/uglify-js/bin/uglifyjs"
+
 ENTRY_POINT_JS_FILE="$CLIENT_JS_DIR/app.js"
 BUNDLE_JS_FILE="$DIST_JS_DIR/bundle.js"
+BUNDLE_MIN_JS_FILE="$DIST_JS_DIR/bundle.min.js"
 
 if [ ! -d "$DIST_JS_DIR" ]; then
     mkdir -p "$DIST_JS_DIR"
@@ -14,6 +19,20 @@ if [ -f "$BUNDLE_JS_FILE" ]; then
     rm "$BUNDLE_JS_FILE"
 fi
 
+if [ -f "$BUNDLE_MIN_JS_FILE" ]; then
+    rm "$BUNDLE_MIN_JS_FILE"
+fi
+
 echo "Bundling JavaScript..."
-browserify -o dist/js/bundle.js client/js/app.js
-echo "$BUNDLE_JS_FILE written."
+
+$BROWSERIFY_BIN -o dist/js/bundle.js client/js/app.js
+
+case $ENV in
+    "prod" | "production")
+        $UGLIFY_BIN $BUNDLE_JS_FILE > $BUNDLE_MIN_JS_FILE
+        echo "$BUNDLE_MIN_JS_FILE written and minified."
+        ;;
+    *)
+        echo "$BUNDLE_JS_FILE written."
+        ;;
+esac
