43adb6d7a62bd02862f557efb53afe0cbc8346ab
Laurent Dezitter <laurent.dezitter@gmail.com>
2016-11-04 17:38:03 +0100

    fix tweet search factory
    'text' was not sent

--- client/js/core/tweet-search/tweet-search.factory.js
+++ client/js/core/tweet-search/tweet-search.factory.js
@@ -1,7 +1,11 @@
 function TweetSearchFactory($http) {
     return {
         get: function(text) {
-            return $http.get('/api/search', { text: text });
+            return $http.get('/api/search', {
+                params: {
+                    text: text
+                }
+            });
         }
     };
 }
